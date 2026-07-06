b58a7bd897694d4d2d74365adfee11e19e307e7f
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2026-07-06 22:04:03 +0700

    add "Shooter" character
    - add "GameMode", "PlayerController" for "Shooter"

---
+++ Source/SpaceShooter/Public/Controller/ShooterController.h
@@ -0,0 +1,17 @@
+// Copyright by Hungne
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ShooterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACESHOOTER_API AShooterController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
